5b3568da338e2f65bdcf11abf91810120faff083
William(addicted2crypto) <addicted2krypto@gmail.com>
2025-03-03 18:30:31 -0800

    ENDPOINT

--- app/api/ollama/route.ts
+++ app/api/ollama/route.ts
@@ -3,7 +3,9 @@ import { NextResponse } from "next/server"
 export async function POST(req: Request) {
   try {
     const body = await req.json()
-    const ollamaResponse = await fetch("ai.ainetguard.com/api/chat", {
+
+    //add any endpoint for you LLM here
+    const ollamaResponse = await fetch("localhost://11434/api/chat", {
       method: "POST",
       mode: "no-cors",
       headers: {
